1af251ddda2ff1cd182dfb5bc6256758e1a7dabf
Herrelaekker <bocaj317@gmail.com>
2020-11-22 16:05:44 +0100

    rykket unitdisplaygrid

--- testmappe/unitDisplayGrid.py
+++ testmappe/unitDisplayGrid.py
@@ -10,7 +10,7 @@ columns = 3
 rows = np.ceil(len(images))
 
 for x, i in enumerate(images):
-    path =  os.path.join("./ExamplePictures/",i)
+    path =  os.path.join("../ExamplePictures/", i)
     img = plt.imread(path)
     fig.add_subplot(rows, columns, x+1)
     plt.axis('off')
